d5f71c768b7af608a263f151c96819ff2729594a
ptavarez16 <ptavarez16@yahoo.com>
2018-04-14 21:35:47 -0400

    Remove semicolon from services route

--- app/router.js
+++ app/router.js
@@ -11,7 +11,7 @@ Router.map(function () {
   this.route('sign-in')
   this.route('change-password')
   this.route('users')
-  this.route('services');
+  this.route('services')
 })
 
 export default Router
