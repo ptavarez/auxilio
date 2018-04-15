58c7bc865a31b34461ca2b31857b5f1ca09abe8d
ptavarez16 <ptavarez16@yahoo.com>
2018-04-14 21:42:05 -0400

    Route to find all services - Add code to find all services - Add empty actions section for any future actions

--- app/routes/services.js
+++ app/routes/services.js
@@ -1,4 +1,11 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'
 
 export default Route.extend({
-});
+  model () {
+    return this.get('store').findAll('service')
+  },
+
+  actions: {
+    // add actions within this route here
+  }
+})
