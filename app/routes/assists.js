10edf2e06991503b19c2af6f03adfa222b26b582
ptavarez16 <ptavarez16@yahoo.com>
2018-04-14 23:53:52 -0400

    Add route to find all assists
    - File finds all assist
    - Empty actions added

--- app/routes/assists.js
+++ app/routes/assists.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route'
 
 export default Route.extend({
+  model () {
+    return this.get('store').findAll('assist')
+  },
+
+  actions: {
+    // add actions within this route here
+  }
 })
