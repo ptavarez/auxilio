a01250b7316fbe8c55cde1f716b6b9736e5d883e
ptavarez16 <ptavarez16@yahoo.com>
2018-04-16 12:19:21 -0400

    Add requestAssist action
    - Add action to pass down to services hbs when component is called
    - Assist object being passed over

--- app/components/request-service.js
+++ app/components/request-service.js
@@ -1,4 +1,13 @@
-import Component from '@ember/component';
+import Component from '@ember/component'
 
 export default Component.extend({
-});
+  assist: {
+    service: 'Test Service'
+  },
+
+  actions: {
+    requestAssist () {
+      this.sendAction('request', this.get('assist'))
+    }
+  }
+})
