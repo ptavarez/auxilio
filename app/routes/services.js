0a81759b4f29b0de20bbe9cc9e9c1d384c997a6c
ptavarez16 <ptavarez16@yahoo.com>
2018-04-16 12:26:14 -0400

    Add requestAssist action
    - Action creates a record 'assist
    - Currently hard coding assist.service
    
    TODO find a way of adding relevant service.name to assist.service

--- app/routes/services.js
+++ app/routes/services.js
@@ -2,10 +2,13 @@ import Route from '@ember/routing/route'
 
 export default Route.extend({
   model () {
-    return this.get('store').findAll('service')
+    return this.get('store').findAll('service', 'assist')
   },
 
   actions: {
-    // add actions within this route here
+    requestAssist (assist) {
+      this.get('store').createRecord('assist', assist)
+        .save()
+    }
   }
 })
