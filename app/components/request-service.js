221fc7cf89583f970ceee401a7a40cb09494cc17
ptavarez16 <ptavarez16@yahoo.com>
2018-04-16 16:28:06 -0400

    Remove console.log

--- app/components/request-service.js
+++ app/components/request-service.js
@@ -4,7 +4,6 @@ export default Component.extend({
 
   actions: {
     requestAssist (service) {
-      console.log('Service is', service.id)
       this.sendAction('request', { service_id: service.id })
     }
   }
