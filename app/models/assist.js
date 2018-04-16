128390395e1218fbd93508fa444df738a56a3b3d
ptavarez16 <ptavarez16@yahoo.com>
2018-04-16 14:47:59 -0400

    Assist model modification
    - Remove attribute type string from service
    - Add service_id

--- app/models/assist.js
+++ app/models/assist.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data'
 
 export default DS.Model.extend({
-  service: DS.attr('string'),
+  service_id: DS.attr(),
+  service: DS.attr(),
   fulfilled: DS.attr('boolean'),
   created_at: DS.attr('date'),
   editable: DS.attr('boolean')
