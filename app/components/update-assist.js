94dc993a5b17c5b1c9a355b2458bbfbcda99f1e4
ptavarez16 <ptavarez16@yahoo.com>
2018-04-15 20:23:57 -0400

    Add action updateAssist
    - Action sends over the data over to the assist hbs

--- app/components/update-assist.js
+++ app/components/update-assist.js
@@ -1,4 +1,14 @@
-import Component from '@ember/component';
+import Component from '@ember/component'
 
 export default Component.extend({
-});
+  assist: {
+    service: ''
+  },
+
+  actions: {
+    updateAssist () {
+      this.sendAction('update', this.get('assist'))
+      // this.set('assist', {})
+    }
+  }
+})
