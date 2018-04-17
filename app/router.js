76fb33a024bbcddcfde05d0915451e54c6f0b431
ptavarez16 <ptavarez16@yahoo.com>
2018-04-17 15:48:11 -0400

    Ember generate route automotive
    - Add automotive route to render all services with the category of
    automotive

--- app/router.js
+++ app/router.js
@@ -11,12 +11,13 @@ Router.map(function () {
   this.route('sign-in')
   this.route('change-password')
   this.route('users')
-  this.route('services')
+  this.route('services', function () {})
   this.route('service', { path: '/services/:service_id' })
   this.route('assists')
   this.route('assist', { path: '/assists/:assist_id' })
   this.route('landing', { path: '/' })
-  this.route('delivery')
+  this.route('delivery', { path: '/services/delivery' })
+  this.route('automotive', { path: '/services/automotive' })
 })
 
 export default Router
