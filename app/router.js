955ca371448bbb5b59b6023e41e2aa9bdf58ebb3
ptavarez16 <ptavarez16@yahoo.com>
2018-04-18 16:28:31 -0400

    Add completed-assists route and minor tweeks
    - Add route to completed assists
    - Modify assists routes so that aux renders in the browser

--- app/router.js
+++ app/router.js
@@ -13,8 +13,8 @@ Router.map(function () {
   this.route('users')
   this.route('services', function () {})
   this.route('service', { path: '/services/:service_id' })
-  this.route('assists')
-  this.route('assist', { path: '/assists/:assist_id' })
+  this.route('assists', { path: '/auxes' })
+  this.route('assist', { path: '/auxes/:assist_id' })
   this.route('landing', { path: '/' })
   this.route('delivery', { path: '/services/delivery' })
   this.route('automotive', { path: '/services/automotive' })
@@ -24,6 +24,7 @@ Router.map(function () {
   this.route('geek', { path: '/services/geek' })
   this.route('lawncare', { path: '/services/lawncare' })
   this.route('petcare', { path: '/services/petcare' })
+  this.route('completed-assists', { path: '/auxes/completed' })
 })
 
 export default Router
